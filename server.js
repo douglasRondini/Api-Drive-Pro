cd8d87f4f12077b84638751d02eb8fca31cb071c
douglas Rondini <douglasfeliperondine@gmail.com>
2026-07-28 15:58:40 -0300

    fix: adiciona servidor do Render no swagger

--- server.js
+++ server.js
@@ -6,3 +6,28 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT} 🚀`)
   console.log(`Documentação disponível em: http://localhost:${PORT}/docs 📄`)
 })
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Agendamentos CNH',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: '/', // 👈 Recomendado: faz as requisições no mesmo domínio em que o Swagger estiver aberto
+        description: 'Servidor Atual',
+      },
+      {
+        url: 'https://api-drive-pro.onrender.com',
+        description: 'Servidor de Produção (Render)',
+      },
+      {
+        url: 'http://localhost:3000',
+        description: 'Servidor Local',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'], // caminho das suas rotas
+};
