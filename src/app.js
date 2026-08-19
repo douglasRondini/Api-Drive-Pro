36a035dc8bb0c55342cc09b84b2b89fcbe7ae6df
douglas Rondini <douglasfeliperondine@gmail.com>
2026-08-19 16:47:19 -0300

    fix: express 5 cors preflight route

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
-app.options('*', cors())
+app.options(/(.*)/, cors())
 
 app.use(express.json())
 
